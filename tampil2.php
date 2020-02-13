86cff6a2e27199a40505a604b08a1b9d9ad4dfdd
TuanMudaBagas <bagasfd24@gmail.com>
2020-02-13 10:56:07 +0700

    mantap

--- tampil2.php
+++ tampil2.php
@@ -11,6 +11,22 @@
 	<h1>Terbaik</h1>
 	<h3>Mantap Mantap</h3>
 	<p>Lorem, ipsum.</p>
+	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error distinctio culpa similique dignissimos expedita sunt at! Accusamus neque incidunt id optio cumque ducimus eligendi esse, quaerat asperiores quos, dolorem, repellendus reprehenderit! Sapiente, saepe! Eos excepturi, voluptatum ratione impedit ipsa odit, eveniet, sit asperiores eius itaque iusto a dolorem! Distinctio, labore blanditiis? Natus nihil, atque maiores numquam corporis exercitationem eos dignissimos, minima voluptas, eveniet error illum porro architecto ea voluptatibus aperiam aut. Modi consectetur quisquam illo, culpa molestias voluptatem? Exercitationem fugiat sint iure ipsam iusto nostrum veniam fugit atque earum vel enim ad nulla vitae porro, nesciunt eligendi amet, maiores omnis odit natus dolorum! Alias quam vitae voluptatem fugit dolor perferendis repudiandae omnis perspiciatis voluptas culpa eveniet, illum assumenda cupiditate modi. Sunt vero aliquid ex. Voluptates, sint magni sit voluptatum assumenda provident obcaecati voluptate quisquam quia odio, optio praesentium eius suscipit cupiditate quidem velit tenetur ea unde! Incidunt, architecto hic, eveniet possimus minus amet, soluta voluptate eius accusantium assumenda culpa. Dolore molestiae quasi at, eaque rem aspernatur nisi ea praesentium. Quaerat labore nihil blanditiis officia nobis dicta odit repellat tempore ducimus, odio sint accusantium natus reiciendis dolore, illum temporibus molestiae provident cumque ad, eveniet veritatis. Amet asperiores placeat possimus modi odit!</p>
+
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae soluta animi aut quibusdam asperiores quaerat sequi laudantium voluptatem, optio ab dolorum ut pariatur aliquam obcaecati porro a atque mollitia sapiente ipsum perferendis? Temporibus a nam laborum totam magnam tenetur inventore, cum placeat, necessitatibus quia cumque? Possimus corporis aspernatur sequi maiores rerum minima aperiam facere aliquam cum fugit, harum incidunt provident id commodi voluptas velit ex sit architecto tempore ullam ipsum vel? Mollitia, quas expedita qui rem nam porro unde autem distinctio corrupti voluptas ex vero, repellat tempora quia ab recusandae.</p>
+
+
+	<table>
+		<th>
+		<td>Salom</td>
+		<td>Salom</td>
+		</th>
+		<tr>
+			<td>ok</td>
+			<td>ok</td>
+		</tr>
+	</table>
+
 </body>
 
 </html>
