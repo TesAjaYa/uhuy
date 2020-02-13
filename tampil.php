4999f7035bb6270c657e8abe0f8c79cc988fcf2c
RiyanNugraha <yanzcode@gmail.com>
2020-02-13 10:38:54 +0700

    aw

--- tampil.php
+++ tampil.php
@@ -1,12 +1,16 @@
  <!DOCTYPE html>
  <html lang="en">
+
  <head>
      <meta charset="UTF-8">
      <title>Document</title>
  </head>
- <body>
-     
-     <?= 'Selamat datang '.$_POST['username'] ?>
 
+ <body>
+
+     <?= 'Selamat datang S ' . $_POST['username'] ?>
+
+<h2>Bacod</h2>
  </body>
+
  </html>
