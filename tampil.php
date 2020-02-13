f021d6590942f92201966ee0c300f5680cbd8949
TuanMudaBagas <bagasfd24@gmail.com>
2020-02-13 10:23:36 +0700

    tes

--- tampil.php
+++ tampil.php
@@ -1,12 +1,15 @@
  <!DOCTYPE html>
  <html lang="en">
+
  <head>
      <meta charset="UTF-8">
      <title>Document</title>
  </head>
+
  <body>
-     
-     <?= 'Selamat datang '.$_POST['username'] ?>
+
+     <?= 'Selamat datang S ' . $_POST['username'] ?>
 
  </body>
+
  </html>
