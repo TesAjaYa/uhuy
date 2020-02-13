1a35ea5ba6199de4a15de7c223e35c9c5d2869aa
TuanMudaBagas <bagasfd24@gmail.com>
2020-02-13 10:52:46 +0700

    ok

--- tampil.php
+++ tampil.php
@@ -1,30 +1,26 @@
  <!DOCTYPE html>
  <html lang="en">
+
  <head>
      <meta charset="UTF-8">
      <title>Document</title>
  </head>
-<<<<<<< HEAD
  <style>
-     h1{
-        padding: 5px;
-        background: red;
+     h1 {
+         padding: 5px;
+         background: red;
      }
  </style>
+
  <body>
-     
-     <?= 'Selamat datang '.$_POST['username'] ?>
+
+     <?= 'Selamat datang ' . $_POST['username'] ?>
 
 
-<h1> Hayyyy Janco</h1>
+     <h1> Hayyyy Janco</h1>
 
 
 
-=======
- <body>
-     
-     <?= 'Selamat datang '.$_POST['username'] ?>
-
->>>>>>> fc2fdf08fa8c223185c54d2dfcdb1ab85dfc28aa
  </body>
+
  </html>
